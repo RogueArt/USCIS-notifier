5034da13ce7bdb18b8a8dfcc5a8d5e307a1c8e70
RogueArt <altoraries@gmail.com>
2021-08-15 10:34:28 -0700

    Changed output and input to use data.json rather than a text file with IDs

--- src/json.rs
+++ src/json.rs
@@ -2,15 +2,15 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct CaseStatus {
-  id: String,
-  name: String,
-  case_status: String,
-  last_checked: String,
-  previous_status: String,
+  pub id: String,
+  pub name: String,
+  pub previous_status: String,
+  pub last_checked: String,
 }
 
-use std::fs::{File};
+use std::fs::{File, OpenOptions};
 use std::path::Path;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -25,7 +25,6 @@ pub fn read_info_from_file(file_path: &str) -> Vec<CaseStatus> {
     let default_case_status: CaseStatus = CaseStatus {
       id: "AAA00000".to_owned(),
       name: "Your Name".to_owned(),
-      case_status: "Unknown".to_owned(),
       last_checked: "Never".to_owned(),
       previous_status: "".to_owned(),
     };
@@ -45,11 +44,6 @@ pub fn read_info_from_file(file_path: &str) -> Vec<CaseStatus> {
   // If file exists, attempt to read it as JSON
   let case_statuses: Vec<CaseStatus> = serde_json::from_reader(reader).expect("Couldn't parse JSON");
 
-  // TO-DO: Remove this, used for debugging only
-  for case_status in case_statuses.iter() {
-    println!("{:#?}", case_status);
-  }
-
   case_statuses
 }
 
@@ -61,13 +55,9 @@ pub fn save_info_to_file(file_path: &str, statuses: Vec<CaseStatus>) {
   }
 
   // Open the file
-  let mut file = File::open(file_path).expect("Couldn't open file");
+  let mut file = OpenOptions::new().write(true).open(file_path).expect("Couldn't open file");
 
   // Save data to file
   let statuses_as_string = serde_json::to_string_pretty(&statuses).unwrap();
   file.write_all(statuses_as_string.as_bytes()).expect("Couldn't write to file.");
-}
-
-fn has_status_changed(status: &CaseStatus) -> bool {
-  status.case_status != status.previous_status
 }
