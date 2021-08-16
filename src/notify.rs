d5d155196dcdc479aec83703532505d65fa9e2f5
RogueArt <altoraries@gmail.com>
2021-08-16 11:22:21 -0700

    Added feature to notify when case status changes

--- src/notify.rs
+++ src/notify.rs
@@ -1,10 +1,20 @@
-use notify_rust::Notification;
+use notify_rust::{Notification, Hint};
 
-pub fn send_notification(statuses: Vec<CaseStatus>) {
+use crate::CaseStatus;
+
+pub fn send_notification(case_status: &CaseStatus, current_status: &str) {
+  let formatted_status = format_status(case_status, current_status);  
   Notification::new()
-    .summary("Case Status Updates")
-    .body(statuses)
-    .icon("thunderbird")
-    .show()
-    .expect("Could not send notification.");
+        .summary("Case Status Updates")
+        .body(&formatted_status)
+        .icon("thunderbird")
+        .show()
+        .expect("Could not send notification.");
+}
+
+fn format_status(case_status: &CaseStatus, current_status: &str) -> String {
+    format!(
+        "{}'s case status for {} changed from {} to {}.",
+        case_status.name, case_status.id, case_status.previous_status, current_status
+    )
 }
